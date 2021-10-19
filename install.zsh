063e451ec7dc9f9b7b80467599457886e4772347
José Morano <j.morano@udc.es>
2021-10-20 01:36:23 +0200

    Solved bug on Zsh installer.

--- install.zsh
+++ install.zsh
@@ -2,9 +2,9 @@
 
 cargo install --path zc
 mkdir -p ~/.local/share/z/
-cp z.sh ~/.local/share/z/
+cp z.zsh ~/.local/share/z/
 # If the alias has not already been added, add it
 if ! grep -q "Contemporary-z" ~/.zshrc; then
     printf "\n# Alias for the Contemporary-z program\n" >> ~/.zshrc
-    printf "alias z='. ~/.local/share/z/z.sh'\n" >> ~/.zshrc
+    printf "alias z='. ~/.local/share/z/z.zsh'\n" >> ~/.zshrc
 fi
