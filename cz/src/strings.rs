3c230edae984876bfbb845c4c1a036d88b0ff87d
morano <j.morano@udc.es>
2022-07-10 20:13:17 +0200

    Updated help.

--- cz/src/strings.rs
+++ cz/src/strings.rs
@@ -13,16 +13,16 @@ Usage: z [OPTION]... [DIRECTORY]...
 Mandatory arguments to long options are mandatory for short options too.
   -                          go to the previous directory
   =                          go to the current directory
-  -a                         add directory alias; if only the directory is
+  -a ALIAS [DIRECTORY]       add directory alias; if only the directory is
                                introduced, its alias is removed
-  -b                         execute shell command in background
+  -b COMMAND                 execute shell COMMAND in background
       --clear                clear the directories database
   -i                         interactive selection (using a numbered list) of
                                the subdirectories of the current directory.
   -l[=NUMBER]                list a certain NUMBER of directories by 'frecency';
                                if no NUMBER is provided, the max_results number
                                from configuration is used.
-  -r=DIRECTORY               remove DIRECTORY from the database
+  -r                         remove a directory from the database, interactively
       --help     display this help and exit
 
 Exit status:
